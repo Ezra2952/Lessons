1289fe57e65f3b46d448b81c2741d5933556ab9d
Kyaw Soe Naing <kyawsoenaing@my.jcu.edu.au>
2019-12-04 15:28:52 +0800

    Refactor password check program to use functions

--- password_check2.py
+++ password_check2.py
@@ -1,12 +1,21 @@
 MINIMUM_LENGTH = 4
 
 
-def ask_password():
+def main():
+    password = get_password()
+    print_asterisks(password)
+
+
+def get_password():
     password = input("Enter password of minimum {} characters: ".format(MINIMUM_LENGTH))
     while len(password) < MINIMUM_LENGTH:
+        print("Password is too short")
         password = input("Enter password of minimum {} characters: ".format(MINIMUM_LENGTH))
+    return password
 
+
+def print_asterisks(password):
     print('*' * len(password))
 
 
-ask_password()
+main()
